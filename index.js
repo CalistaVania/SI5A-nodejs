adad54a5a79d8e9b4eeb8544af3a4d9333f2771b
CalistaVania <161103856+CalistaVania@users.noreply.github.com>
2024-09-20 11:01:58 +0700

    2

--- index.js
+++ index.js
@@ -3,10 +3,16 @@ const http = require('http');
 
 // buat server
 const server = http.createServer( (req, res)=> {
-    res.writeHead(200, { 'Content-Type' : 'text/html'});
-    res.write('Selamat pagi');
+    // res.writeHead(200, { 'Content-Type' : 'text/html'});
+    // res.write('Selamat pagi');
+
+    res.writeHead(200, { 'Content-Type' : 'application/json'});
+    res.write(JSON.stringify ({
+        'message' : 'selamat pagi',
+        'status' : 'success'
+    }));
     res.end();
-});
+} );
 
 const port = 3000;
 const host = 'localhost';
